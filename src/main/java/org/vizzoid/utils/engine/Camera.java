c1672e3000caf6e5e7391c7e8235534cffc0884a
Vizzoid <82728229+Vizzoid@users.noreply.github.com>
2023-01-27 13:15:14 -0500

    docs and comments

--- src/main/java/org/vizzoid/utils/engine/Camera.java
+++ src/main/java/org/vizzoid/utils/engine/Camera.java
@@ -1,9 +1,13 @@
 package org.vizzoid.utils.engine;
 
+/**
+ * Camera represents the user's view and from where they can see. It contains multiple settings such as rendering distance, zNear, and fov, but it also includes
+ * useful content such as scaling factor and projection calculations which are calculated beforehand to be used repeatedly without unnecessary computation power
+ */
 public class Camera {
 
     private double scalingFactor;
-    private double renderingDistance;
+    private double renderingDistance; // zFar
     private double zNear;
     private double projectionMultiplier;
     private double projectionSubtractive;
@@ -20,9 +24,13 @@ public class Camera {
         return fov;
     }
 
+    /**
+     * Sets fov of camera, also calculates scaling factor before hand and stores it to be used without computing during paint process
+     */
     public void setFov(double fov) {
         this.fov = fov;
         this.scalingFactor = 1 / Math.tan(Math.toRadians(fov) / 2); // cotangent
+        // scaling factor is used so as the fov increases the point moves closer to off screen and vice-versa
     }
 
     public double getScalingFactor() {
@@ -47,9 +55,17 @@ public class Camera {
         reloadProjection();
     }
 
+    /**
+     * Reloads projection calculations which are used to project 3d point onto 2d plane (screen)
+     */
     public void reloadProjection() {
         this.projectionMultiplier = (renderingDistance / (renderingDistance - zNear));
         this.projectionSubtractive = (zNear * -projectionMultiplier);
+        
+        // calculations of z
+        // z * projectionMultiplier - projectionSubtractive
+        // (z * (renderingDistance / (renderingDistance - zNear))) - (zNear * -(renderingDistance / (renderingDistance - zNear)))
+        // (z * (zFar / (zFar - zNear))) - (zNear * -(zFar / (zFar - zNear)))
     }
 
     public double getProjectionMultiplier() {
