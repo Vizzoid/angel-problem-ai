71a008fc50bd4562838e94c68f3fcdec3a11138f
Vizzoid <82728229+Vizzoid@users.noreply.github.com>
2023-01-28 11:52:19 -0500

    lighting, polygon development, defer draw

--- src/main/java/org/vizzoid/utils/engine/Triangle.java
+++ src/main/java/org/vizzoid/utils/engine/Triangle.java
@@ -1,8 +1,11 @@
 package org.vizzoid.utils.engine;
 
+import org.jetbrains.annotations.Nullable;
+import org.vizzoid.utils.position.ImmoveablePosition;
 import org.vizzoid.utils.position.Position;
 
 import java.awt.*;
+import java.util.Arrays;
 
 /**
  * 3 pointed 3d object triangle
@@ -13,6 +16,14 @@ public class Triangle implements Object3D {
     private final Position pos2;
     private final Position pos3;
 
+    public Triangle(double x1, double y1, double z1, double x2, double y2, double z2, double x3, double y3, double z3) {
+        this(
+                new ImmoveablePosition(x1, y1, z1),
+                new ImmoveablePosition(x2, y2, z2),
+                new ImmoveablePosition(x3, y3, z3)
+        );
+    }
+
     public Triangle(Position pos1, Position pos2, Position pos3) {
         this.pos1 = pos1.immoveable();
         this.pos2 = pos2.immoveable();
@@ -20,7 +31,7 @@ public class Triangle implements Object3D {
         // potentially this polygon can be created earlier and used consistently?
         // perhaps a tracked polygon which will reset and reinitialize when there is a significant change in engine or camera?
         /* could be:
-        this.polygon = engine.addIfAbsent(new TrackedPolygon() { // engine.addIfAbsent does not add if already added
+        this.polygon = engine.addIfAbsent(new TrackedPolygon() /* extends ColoredPolygon *//* { // engine.addIfAbsent does not add if already added
             /**
              * called on fov, rendering, position, angle change, or other reason
              */
@@ -46,25 +57,48 @@ public class Triangle implements Object3D {
 
     @Override
     public void draw(Graphics graphics, Engine3D engine) {
-        Polygon polygon = createPolygon(engine, new Polygon());
-        graphics.drawPolygon(polygon);
+        ColoredPolygon fill = createPolygon(engine, new ColoredPolygon());
+        // potentially this polygon can be created earlier and used consistently? see above
+        if (fill == null) return;
+        fill.mode = FillMode.FILL;
+        //ColoredPolygon outline = fill.withContrast();
+        //outline.mode = FillMode.OUTLINE;
+
+        engine.queue(fill);
+        //engine.queue(outline);
     }
 
-    protected Polygon createPolygon(Engine3D engine, Polygon polygon) {
+    protected @Nullable ColoredPolygon createPolygon(Engine3D engine, ColoredPolygon polygon) {
         Position pos2d1 = engine.prepareProjection(pos1);
         Position pos2d2 = engine.prepareProjection(pos2);
         Position pos2d3 = engine.prepareProjection(pos3);
 
-        if (pos2d1.normal(pos2d2, pos2d3).getZ() >= 0) return polygon; // do not load triangle if unseeable
+        Position normalVector = pos2d1.normal(pos2d2, pos2d3);
+        Position cameraPosition = engine.getCamera().getPosition();
+        Position cameraLine = cameraPosition.line(pos2d1);
+
+        if (normalVector.dotProduct(cameraLine) >= 0) return null; // do not load triangle if unseeable
 
         pos2d1 = engine.completeProjection(pos2d1);
         pos2d2 = engine.completeProjection(pos2d2);
         pos2d3 = engine.completeProjection(pos2d3);
 
-        polygon.xpoints = new int[]{(int) pos2d1.getX(), (int) pos2d2.getX(), (int) pos2d3.getX()};
-        polygon.ypoints = new int[]{(int) pos2d1.getY(), (int) pos2d2.getY(), (int) pos2d3.getY()};
-        polygon.npoints = 3;
+        polygon.load(pos2d1, pos2d2, pos2d3);
+
+        polygon.color = color(normalVector.dotProduct(engine.getLightDirection()));
         return polygon;
+    }
+
+    /**
+     * lighting
+     */
+    private Color color(double dotProduct) {
+        return new Color(switch ((int) (13 * dotProduct)) {
+            case 1, 2, 3, 4 -> Color.BLACK.getRGB() | Color.DARK_GRAY.getRGB();
+            case 5, 6, 7, 8 -> Color.DARK_GRAY.getRGB() | Color.GRAY.getRGB();
+            case 9, 10, 11, 12 -> Color.GRAY.getRGB() | Color.WHITE.getRGB();
+            default -> Color.BLACK.getRGB() | Color.BLACK.getRGB();
+        });
     }
 
 
