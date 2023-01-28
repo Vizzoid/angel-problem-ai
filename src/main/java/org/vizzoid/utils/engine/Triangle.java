41622f4c34d30f803ed3f4967904e79e2998f307
Vizzoid <82728229+Vizzoid@users.noreply.github.com>
2023-01-27 19:03:03 -0500

    compartmentalized, preload awt.Polygon?

--- src/main/java/org/vizzoid/utils/engine/Triangle.java
+++ src/main/java/org/vizzoid/utils/engine/Triangle.java
@@ -4,6 +4,9 @@ import org.vizzoid.utils.position.Position;
 
 import java.awt.*;
 
+/**
+ * 3 pointed 3d object triangle
+ */
 public class Triangle implements Object3D {
 
     private final Position pos1;
@@ -14,6 +17,19 @@ public class Triangle implements Object3D {
         this.pos1 = pos1;
         this.pos2 = pos2;
         this.pos3 = pos3;
+        // potentially this polygon can be created earlier and used consistently?
+        // perhaps a tracked polygon which will reset and reinitialize when there is a significant change in engine or camera?
+        /* could be: 
+        this.polygon = engine.addIfAbsent(new TrackedPolygon() { // engine.addIfAbsent does not add if already added
+            /**
+             * called on fov, rendering, position, angle change, or other reason
+             */
+        /*
+            public void reset(Engine3D engine) {
+                createPolygon(engine, this);
+            }
+        })
+        */
     }
 
     public Position getPos1() {
@@ -30,15 +46,20 @@ public class Triangle implements Object3D {
 
     @Override
     public void draw(Graphics graphics, Engine3D engine) {
+        Polygon polygon = createPolygon(engine, new Polygon());
+        graphics.drawPolygon(polygon);
+    }
+    
+    protected Polygon createPolygon(Engine3D engine, Polygon polygon) {
         Position pos2d1 = engine.mutate3dTo2d(pos1);
         Position pos2d2 = engine.mutate3dTo2d(pos2);
         Position pos2d3 = engine.mutate3dTo2d(pos3);
-
-        Polygon polygon = new Polygon();
+        
         polygon.xpoints = new int[]{(int) pos2d1.getX(), (int) pos2d2.getX(), (int) pos2d3.getX()};
         polygon.ypoints = new int[]{(int) pos2d1.getY(), (int) pos2d2.getY(), (int) pos2d3.getY()};
         polygon.npoints = 3;
-
-        graphics.drawPolygon(polygon);
+        return polygon;
     }
+    
+    
 }
