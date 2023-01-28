5e85493b0b391c30dc618e2e3343d3daa8692371
Vizzoid <82728229+Vizzoid@users.noreply.github.com>
2023-01-27 21:34:51 -0500

    added useful vector and position calculations
    utility

--- src/main/java/org/vizzoid/utils/position/Position.java
+++ src/main/java/org/vizzoid/utils/position/Position.java
@@ -15,4 +15,49 @@ public interface Position {
 
     ImmoveablePosition immoveable();
 
+    default MoveablePosition normal(Position position1, Position position2) {
+        Position line1 = line(position1);
+        Position line2 = line(position2);
+        MoveablePosition normal = line1.crossProduct(line2);
+
+        return normal.normalize();
+    }
+
+    default MoveablePosition crossProduct(Position position) {
+        MoveablePosition crossProduct = new MoveablePosition();
+        crossProduct.setX(getY() * position.getZ() - getZ() * position.getY());
+        crossProduct.setY(getZ() * position.getX() - getX() * position.getZ());
+        crossProduct.setZ(getX() * position.getY() - getY() * position.getX());
+
+        return crossProduct;
+    }
+
+    default MoveablePosition line(Position position) {
+        MoveablePosition line = new MoveablePosition();
+        line.setX(position.getX() - getX());
+        line.setY(position.getY() - getY());
+        line.setZ(position.getZ() - getZ());
+
+        return line;
+    }
+
+    default MoveablePosition normalize() {
+        MoveablePosition position = moveable();
+        double length = length();
+
+        position.setX(position.getX() / length);
+        position.setY(position.getY() / length);
+        position.setZ(position.getZ() / length);
+
+        return position;
+    }
+
+    default double length() {
+        double x = getX();
+        double y = getY();
+        double z = getZ();
+
+        return Math.sqrt(x * x + y * y + z * z);
+    }
+
 }
