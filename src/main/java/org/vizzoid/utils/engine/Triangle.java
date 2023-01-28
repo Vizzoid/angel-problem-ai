b1985c2c22cd7fdd23dcdc4015472c7f6c792e6e
Vizzoid <82728229+Vizzoid@users.noreply.github.com>
2023-01-27 21:49:27 -0500

    does not load unnecessary sides

--- src/main/java/org/vizzoid/utils/engine/Triangle.java
+++ src/main/java/org/vizzoid/utils/engine/Triangle.java
@@ -14,12 +14,12 @@ public class Triangle implements Object3D {
     private final Position pos3;
 
     public Triangle(Position pos1, Position pos2, Position pos3) {
-        this.pos1 = pos1;
-        this.pos2 = pos2;
-        this.pos3 = pos3;
+        this.pos1 = pos1.immoveable();
+        this.pos2 = pos2.immoveable();
+        this.pos3 = pos3.immoveable();
         // potentially this polygon can be created earlier and used consistently?
         // perhaps a tracked polygon which will reset and reinitialize when there is a significant change in engine or camera?
-        /* could be: 
+        /* could be:
         this.polygon = engine.addIfAbsent(new TrackedPolygon() { // engine.addIfAbsent does not add if already added
             /**
              * called on fov, rendering, position, angle change, or other reason
@@ -49,17 +49,23 @@ public class Triangle implements Object3D {
         Polygon polygon = createPolygon(engine, new Polygon());
         graphics.drawPolygon(polygon);
     }
-    
+
     protected Polygon createPolygon(Engine3D engine, Polygon polygon) {
-        Position pos2d1 = engine.mutate3dTo2d(pos1);
-        Position pos2d2 = engine.mutate3dTo2d(pos2);
-        Position pos2d3 = engine.mutate3dTo2d(pos3);
-        
+        Position pos2d1 = engine.prepareProjection(pos1);
+        Position pos2d2 = engine.prepareProjection(pos2);
+        Position pos2d3 = engine.prepareProjection(pos3);
+
+        if (pos2d1.normal(pos2d2, pos2d3).getZ() >= 0) return polygon; // do not load triangle if unseeable
+
+        pos2d1 = engine.completeProjection(pos2d1);
+        pos2d2 = engine.completeProjection(pos2d2);
+        pos2d3 = engine.completeProjection(pos2d3);
+
         polygon.xpoints = new int[]{(int) pos2d1.getX(), (int) pos2d2.getX(), (int) pos2d3.getX()};
         polygon.ypoints = new int[]{(int) pos2d1.getY(), (int) pos2d2.getY(), (int) pos2d3.getY()};
         polygon.npoints = 3;
         return polygon;
     }
-    
-    
+
+
 }
