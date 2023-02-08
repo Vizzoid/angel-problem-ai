e6c1cd705a3aaab7ee5e61eac4a6a9b8fea98349
Vizzoid <82728229+Vizzoid@users.noreply.github.com>
2023-02-08 15:45:28 -0500

    misplaced

--- angelproblemai/Main.java
+++
@@ -1,9 +0,0 @@
-package org.vizzoid.angelproblemai;
-
-import org.vizzoid.angelproblemai.game.AngelProblem;
-
-public class Main {
-    public static void main(String[] args) {
-        new AngelProblem();
-    }
-}
